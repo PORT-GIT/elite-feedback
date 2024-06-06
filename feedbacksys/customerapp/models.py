8a78389eb0f9dcd404f91982ba90bad30cb4c538
PORT-GIT <margaretkairu21@gmail.com>
2024-06-06 16:16:08 +0300

    changed models and downloaded extensions

--- feedbacksys/customerapp/models.py
+++ feedbacksys/customerapp/models.py
@@ -6,5 +6,5 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=10)
     last_name = models.CharField(max_length=10)
-    phone_number = models.SmallIntegerField
+    phone_number = models.
 
