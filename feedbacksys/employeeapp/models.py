f324164874a65aca39fdf780a540187ff14b6e82
PORT-GIT <margaretkairu21@gmail.com>
2024-06-30 09:01:39 +0300

    installed shortuuidfield package this is to allow me to assign unique employee id to the salon employees rather than using the id given by django

--- feedbacksys/employeeapp/models.py
+++ feedbacksys/employeeapp/models.py
@@ -1,26 +1,28 @@
-# from django.db import models
-# from phonenumber_field.modelfields import PhoneNumberField
-# from django.contrib.auth.models import AbstractUser
-# #linking models from customerapp
-# #from ..customerapp.models import customer
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import AbstractUser
+from shortuuid.django_fields import ShortUUIDField
 
-# #Create your models here.
 
-# class User(AbstractUser):
-#     employee_id = models.IntegerField(primary_key=True, on_delete=models.CASCADEA)
-#     first_name = models.CharField(max_length=10)
-#     last_name = models.CharField(max_length=10)
-#     phone = PhoneNumberField()
-#     profile_image = models.ImageField(default="fallback.png", upload_to="proflile-images", blank=True)
+#Create your models here.
 
-#     USERNAME_FIELD = "last_name"
-#     REQUIRED_FIELDS = [
-#         'employee_id',
-#         'first_name',
-#         'phone',
-#     ]
+class User(AbstractUser):
+    employee_id = ShortUUIDField(unique=True, length=5, max_length=10, prefix="emp", alphabet="abcdef12345")
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = PhoneNumberField()
+    profile_image = models.ImageField(default="fallback.png", upload_to="proflile-images", blank=True)
 
-#     def __str__(self):
-#         return self.last_name +"   " + self.first_name
+    USERNAME_FIELD = "last_name"
+    REQUIRED_FIELDS = [
+        'employee_id',
+        'first_name',
+        'phone',
+    ]
+
+
+
+    def __str__(self):
+        return self.last_name +"   " + self.first_name
 
 
--- feedbacksys/feedbackapp/models.py
+++ feedbacksys/feedbackapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 #MODELS CREATED BELOW
+#will call on customer model when creating model for feedback
+#e.g customer = models.ForeignKey(Customer, on_delete=CASCADE)
