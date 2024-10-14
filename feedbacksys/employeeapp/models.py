de500e16bf220f29bb7e950fad1e8f4b1e815f41
PORT-GIT <margaretkairu21@gmail.com>
2024-10-14 12:43:50 +0300

    added models to the employeeapp

--- feedbacksys/employeeapp/models.py
+++ feedbacksys/employeeapp/models.py
@@ -1,1 +1,27 @@
 from django.db import models
+from location_field.models.plain import PlainLocationField
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import AbstractUser
+
+
+#creating models for the salon registration
+class Salon(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    location = PlainLocationField(based_fields=['city'], zoom=7)
+    phone_number = PhoneNumberField(blank=False) 
+
+    def __str__(self):
+        return self.name
+    
+
+#creating models for owner/admin of salon registration
+class SalonOwner(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+
+
+
+
+#creating model for stylists registration
+class Stylist(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    
