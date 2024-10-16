2997a2920882551e156029aa97870821f297392f
PORT-GIT <margaretkairu21@gmail.com>
2024-10-16 17:41:36 +0300

    changes in the customerapp model added after the previous git add

--- feedbacksys/customerapp/models.py
+++ feedbacksys/customerapp/models.py
@@ -8,15 +8,10 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 class Customer(models.Model):
     first_name = models.CharField(max_length=10, blank=False)
     last_name = models.CharField(max_length=10, blank=False)
-    phone_number = PhoneNumberField(blank=False)
+    phone_number = PhoneNumberField(blank=False, unique=True)
     email = models.EmailField(max_length=50, null=True)
     date_joined = models.DateField(auto_now_add=True)
     
     def __str__(self):
         return self.first_name +"   " + self.last_name
     
-
-    #can use abstractbase user where i define each customer by their phonenumber
-    #AbstractBaseUser  when you need a completely new user model with custom fields (like phone number as the primary identifier).
-    #AbstractBaseUser is a base class that you can use to create your own custom user model
-    #Use BaseUser Manager to manage how users are created, especially if you are using AbstractBaseUser
